c8c10d3441e3c92729a4504ff09f83d064fb770f
2003351J <2003351J@student.tp.edu.sg>
2022-02-04 02:04:55 +0800

    created newProductServlet.java and added in add new product function

--- src/main/java/NewProductServlet.java
+++ src/main/java/NewProductServlet.java
@@ -1,11 +1,13 @@
-
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 /**
  * Servlet implementation class NewProductServlet
@@ -36,6 +38,46 @@ public class NewProductServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
-
+		
+		response.setContentType("text/html"); 
+		//Step 1: Initialize a PrintWriter object to return the html values via the response
+		PrintWriter out = response.getWriter(); 
+		
+		//Step 2: retrieve the four parameters from the request from the web form				
+		String n = request.getParameter("productName");
+		String p = request.getParameter("productImage");
+		String e = request.getParameter("productPrice");
+		String c = request.getParameter("productDescription");
+		
+		//Step 3: attempt connection to database using JDBC, you can change the username and password accordingly using the phpMyAdmin > User Account dashboard
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/products", "root", "password");
+	
+		//Step 4: implement the sql query using prepared statement (https://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html)
+		PreparedStatement ps = con.prepareStatement("insert into PRODUCTS values(?,?,?,?)");
+		
+		//Step 5: parse in the data retrieved from the web form request into the prepared statement accordingly		
+		ps.setString(1, n);
+		ps.setString(2, p);
+		ps.setString(3, e);
+		ps.setString(4, c); 
+		
+		//Step 6: perform the query on the database using the prepared statement
+		int i = ps.executeUpdate();
+		
+		//Step 7: check if the query had been successfully execute, return “You are successfully registered” via the response,
+		if (i > 0){
+			PrintWriter writer = response.getWriter();
+			writer.println("<h1>" + "You have successfully created a product!" + "</h1>");
+			writer.close();
+			}
+			}
+	//Step 8: catch and print out any exception
+			catch (Exception exception) {
+			System.out.println(exception);
+			out.close();
+			}
+		
+		}
 }
--- src/main/java/Products.java
+++ src/main/java/Products.java
@@ -1,7 +1,50 @@
 public class Products {
+	
+	public Products(int productId, String productName, String productImage, String productPrice,
+			String productDescription) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.productImage = productImage;
+		this.productPrice = productPrice;
+		this.productDescription = productDescription;
+	}
+	
 	private int productId;
 	private String productName;
 	private String productImage;
 	private String productPrice;
 	private String productDescription;
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductImage() {
+		return productImage;
+	}
+	public void setProductImage(String productImage) {
+		this.productImage = productImage;
+	}
+	public String getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(String productPrice) {
+		this.productPrice = productPrice;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
 }
