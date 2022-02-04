8743e4cbb9f088d8a6d93a1a2f9d712c89c006f4
jianyilai <2002854D@student.tp.edu.sg>
2022-02-04 16:26:11 +0800

    Added UpdateAccount, DeleteAccount and ShowEditForm functions.

--- src/main/java/AccountServlet.java
+++ src/main/java/AccountServlet.java
@@ -30,7 +30,8 @@ public class AccountServlet extends HttpServlet {
 
 	// Step 2: Prepare list of SQL prepared statements to perform CRUD to our
 	// database
-	private static final String INSERT_USERS_SQL = "INSERT INTO ACCOUNT" + " (username, password, email, userRole) VALUES " + " (?, ?, ?);";
+	private static final String INSERT_USERS_SQL = "INSERT INTO ACCOUNT"
+			+ " (username, password, email, userRole) VALUES " + " (?, ?, ?);";
 	private static final String SELECT_USER_BY_ID = "select username,password,email,userRole from ACCOUNT where username =?";
 	private static final String SELECT_ALL_USERS = "select * from ACCOUNT ";
 	private static final String DELETE_USERS_SQL = "delete from ACCOUNT where username = ?;";
@@ -73,29 +74,28 @@ public class AccountServlet extends HttpServlet {
 		String action = request.getServletPath();
 		try {
 			switch (action) {
-			case "/insert":
+			case "/AccountServlet/delete":
+				deleteAccount(request, response);
 				break;
-			case "/delete":
+			case "/AccountServlet/edit":
+				showEditForm(request, response);
 				break;
-			case "/edit":
+			case "/AccountServlet/update":
+				updateAccount(request, response);
 				break;
-			case "/update":
-				break;
-			default:
+			case "/AccountServlet/dashboard":
 				listAccounts(request, response);
 				break;
 			}
 		} catch (SQLException ex) {
 			throw new ServletException(ex);
 		}
-
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	// Step 5: listAccounts function to connect to the database and retrieve all
 	// accounts records
-	private void listAccounts(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+	private void listAccounts(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
 		List<Account> accounts = new ArrayList<>();
 		try (Connection connection = getConnection();
 
@@ -121,6 +121,77 @@ public class AccountServlet extends HttpServlet {
 		// the accountManagement.jsp
 		request.setAttribute("listAccounts", accounts);
 		request.getRequestDispatcher("/accountManagement.jsp").forward(request, response);
+	}
+
+	// method to get parameter, query database for existing user data and redirect
+	// to user edit page
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ServletException, IOException {
+		// get parameter passed in the URL
+		String username = request.getParameter("username");
+		Account existingAccount = new Account("", "", "", "");
+		// Step 1: Establishing a Connection
+		try (Connection connection = getConnection();
+				// Step 2:Create a statement using connection object
+				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);) {
+			preparedStatement.setString(1, username);
+			// Step 3: Execute the query or update query
+			ResultSet rs = preparedStatement.executeQuery();
+			// Step 4: Process the ResultSet object
+			while (rs.next()) {
+				username = rs.getString("username");
+				String password = rs.getString("password");
+				String email = rs.getString("email");
+				String userRole = rs.getString("userRole");
+				existingAccount = new Account(username, password, email, userRole);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		// Step 5: Set existingUser to request and serve up the userEdit form
+		request.setAttribute("account", existingAccount);
+		request.getRequestDispatcher("/accountEdit.jsp").forward(request, response);
+	}
+
+	// method to update the user table base on the form data
+	private void updateAccount(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException {
+		// Step 1: Retrieve value from the request
+		String oriName = request.getParameter("oriName");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String userRole = request.getParameter("userRole");
+
+		// Step 2: Attempt connection with database and execute update user SQL query
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(UPDATE_USERS_SQL);) {
+			statement.setString(1, username);
+			statement.setString(2, password);
+			statement.setString(3, email);
+			statement.setString(4, userRole);
+			statement.setString(5, oriName);
+			int i = statement.executeUpdate();
+		}
+		// Step 3: redirect back to UserServlet (note: remember to change the url to
+		// your project name)
+		response.sendRedirect("http://localhost:8080/jcommerce/AccountServlet/dashboard");
+	}
+
+	// method to delete user
+	private void deleteAccount(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException {
+		// Step 1: Retrieve value from the request
+		String username = request.getParameter("username");
+		// Step 2: Attempt connection with database and execute delete user SQL query
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);) {
+			statement.setString(1, username);
+			int i = statement.executeUpdate();
+		}
+		// Step 3: redirect back to UserServlet dashboard (note: remember to change the
+		// url to your project name)
+		response.sendRedirect("http://localhost:8080/jcommerce/AccountServlet/dashboard");
 	}
 
 	/**
