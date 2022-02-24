d776d9d0fe18bc2d07ce6e1a9a6dd754f9a34f94
2003351J <2003351J@student.tp.edu.sg>
2022-02-24 21:55:36 +0800

    added productcollection.java for junit testing

---
+++ src/main/java/jcommerce/ProductCollection.java
@@ -0,0 +1,30 @@
+package jcommerce;
+
+import java.util.*;
+
+public class ProductCollection {
+    private ArrayList<Product> products = new ArrayList<>();
+    private int capacity;
+    
+    public ProductCollection() {
+    	/*products.add(new Product("Absolute Holistic Kibbles","https://cdn.shopify.com/s/files/1/1149/5008/products/AbsoluteHolisticKibblesintheBagBeef_LambDogDryFood1_480x480.jpg?v=1612415262","32","Crafted to meet the unique needs of your pets, this recipe provides dogs of all life stages with a high digestibility diet to reduce intestinal issues."));
+    	products.add(new Product("Heka Grain-Free","https://cdn.shopify.com/s/files/1/1149/5008/products/HekaDuck1.8kgFront.png?v=1621506213","24","Heka Pet Food started when we saw a need for healthy pet food that focuses on the healing and well-being of our pets."));*/
+
+        this.capacity = 20;
+    }
+    
+    public ProductCollection(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+    	if(products.size() != capacity) {
+    		products.add(product);
+    	}
+    }
+    
+}
